b96124204e0388a9e909f2d63cd14437274d44ba
SymphonyofTheNight <gamexgaming1997@gmail.com>
2024-11-08 15:57:35 +0800

    footer done

--- core/components/ui/footer/footer.tsx
+++ core/components/ui/footer/footer.tsx
@@ -51,40 +51,44 @@ const Footer = ({
   socialMediaLinks,
   ...props
 }: Props) => (
-  <footer className={cn('2xl:container 2xl:mx-auto', className)} {...props}>
-    <section className="flex flex-col gap-8 border-t border-gray-200 px-4 py-10 sm:px-10 md:flex-row lg:gap-4 lg:px-12 2xl:px-0">
-      <nav className="grid flex-auto auto-cols-fr gap-8 sm:grid-flow-col">
+  <footer className={cn('2xl:container 2xl:mx-auto', className, 'bg-[#4D4D4F] !mx-0 !max-w-[100%] text-white')} {...props}>
+
+    <section className="flex flex-col-reverse gap-8 border-t border-gray-200 py-10 md:flex-row lg:gap-4 2xl:px-0 mb-[35px] max-w-[1440px] w-[92%] mx-auto">
+
+      <nav className="grid flex-auto auto-cols-fr gap-[9rem] sm:grid-flow-col">
         {sections.map((section) => (
-          <div key={section.title}>
-            <h3 className="mb-4 text-lg font-bold">{section.title}</h3>
+          <div className='text-left md:text-right' key={section.title}>
+            <h3 className="mb-4 text-lg uppercase !text-[15px] !font-[400]">{section.title}</h3>
             <ul className="flex flex-col gap-4">
               {section.links.map((link) => (
-                <li key={link.href}>
-                  <CustomLink href={link.href}>{link.label}</CustomLink>
+                <li className='text-left md:text-right' key={link.href}>
+                  <CustomLink className='font-[200] text-[14px]' href={link.href}>{link.label}</CustomLink>
                 </li>
               ))}
             </ul>
           </div>
         ))}
       </nav>
-      <div className="flex flex-col gap-4 md:order-first md:grow">
+
+      <div className="grid items-center justify-center md:items-start md:justify-start md:flex md:flex-col gap-4 md:order-first md:grow">
         {Boolean(logo) && (
           <h3>
             {typeof logo === 'object' ? (
-              <BcImage
-                alt={logo.altText}
-                className="max-h-16 object-contain"
-                height={32}
-                priority
-                src={logo.src}
-                width={155}
-              />
+              // <BcImage
+              //   alt={logo.altText}
+              //   className="max-h-16 object-contain"
+              //   height={32}
+              //   priority
+              //   src={logo.src}
+              //   width={155}
+              // />
+              <img className='h-auto w-[350px] object-contain' src='https://cdn11.bigcommerce.com/s-t0676dlrio/images/stencil/original/image-manager/stormy-kromer-logo-footer-2x.png?t=1731048060' />
             ) : (
               <span className="truncate text-2xl font-black">{logo}</span>
             )}
           </h3>
         )}
-        {Boolean(contactInformation) && (
+        {/* {Boolean(contactInformation) && (
           <>
             <address className="not-italic">
               {contactInformation?.address?.split('\n').map((line) => (
@@ -103,13 +107,13 @@ const Footer = ({
               </a>
             )}
           </>
-        )}
+        )} */}
         {Boolean(socialMediaLinks) && (
-          <nav aria-label="Social media links" className="block">
-            <ul className="flex gap-6">
+          <nav aria-label="Social media links" className="block mt-[25px] md:mt-[28vh]">
+            <ul className="flex gap-3 justify-center md:justify-start">
               {socialMediaLinks?.map((link) => (
-                <li key={link.href}>
-                  <CustomLink className="inline-block" href={link.href} target="_blank">
+                <li className='social-container' key={link.href}>
+                  <CustomLink className="inline-block bg-[#58595B] px-[10px] py-[10px]" href={link.href} target="_blank">
                     {link.icon}
                   </CustomLink>
                 </li>
@@ -118,7 +122,9 @@ const Footer = ({
           </nav>
         )}
       </div>
+
     </section>
+
     <section className="flex flex-col gap-10 border-t border-gray-200 px-4 py-8 sm:gap-8 sm:px-10 sm:py-6 lg:hidden lg:px-12 2xl:px-0">
       <Locale />
 
@@ -128,13 +134,14 @@ const Footer = ({
       </div>
     </section>
 
-    <section className="hidden justify-between gap-8 border-t border-gray-200 px-4 py-6 sm:px-10 lg:flex lg:px-12 2xl:px-0">
+    <section className="hidden justify-between gap-8 border-t border-gray-200 px-4 py-6 sm:px-10 lg:flex lg:px-12 2xl:px-0 max-w-[1440px] w-[92%] mx-auto">
       <p className="text-gray-500 sm:order-first">{copyright}</p>
       <div className="flex gap-8">
         <Locale />
         <div className="flex gap-6">{paymentIcons}</div>
       </div>
     </section>
+
   </footer>
 );
 
